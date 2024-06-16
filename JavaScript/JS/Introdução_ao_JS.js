86042049664c0368b3973344de9ea9f5c2298144
BrianoFelps <briansticknodesbr@gmail.com>
2024-06-16 10:18:09 -0300

    Variáveis JS

--- JavaScript/JS/Introdução_ao_JS.js
+++ JavaScript/JS/Introdução_ao_JS.js
@@ -1,2 +1,9 @@
 alert("Hello world!");
 document.getElementById("input").value = "Aqui está o valor partindo do código JS";
+
+
+// VARIÁVEIS
+var VariavelNumero1 = 1;
+let VariavelQuePodeSerAlterada = "String";
+const VariavelQueNaoPodeSerAlterada = `String`;
+const VariavelArray = ['Valor1', 2, 2.5];
