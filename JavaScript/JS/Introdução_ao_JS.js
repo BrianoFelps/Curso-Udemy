df01ad52adcd72950cb71898b521443bb8cfab43
BrianoFelps <briansticknodesbr@gmail.com>
2024-06-16 10:20:51 -0300

    + Boolean

--- JavaScript/JS/Introdução_ao_JS.js
+++ JavaScript/JS/Introdução_ao_JS.js
@@ -6,4 +6,4 @@ document.getElementById("input").value = "Aqui está o valor partindo do código
 var VariavelNumero1 = 1;
 let VariavelQuePodeSerAlterada = "String";
 const VariavelQueNaoPodeSerAlterada = `String`;
-const VariavelArray = ['Valor1', 2, 2.5];
+const VariavelArray = ['Valor1', 2, 2.5, false, true];
