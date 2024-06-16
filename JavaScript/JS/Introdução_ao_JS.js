80ececc75075c586421facdcdabcd368e3bb0d98
BrianoFelps <briansticknodesbr@gmail.com>
2024-06-16 10:23:04 -0300

    Script com document.write

--- JavaScript/JS/Introdução_ao_JS.js
+++ JavaScript/JS/Introdução_ao_JS.js
@@ -7,3 +7,5 @@ var VariavelNumero1 = 1;
 let VariavelQuePodeSerAlterada = "String";
 const VariavelQueNaoPodeSerAlterada = `String`;
 const VariavelArray = ['Valor1', 2, 2.5, false, true];
+
+document.write(VariavelArray)
