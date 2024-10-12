7d1ccd4817d1232482d99dbd34c2af5150ef6081
BrianoFelps <briansticknodesbr@gmail.com>
2024-10-12 17:11:22 -0300

    Modal, funcoes, objetos, etc

--- Projeto-OrçamentoPessoal/app.js
+++ Projeto-OrçamentoPessoal/app.js
@@ -18,25 +18,61 @@ class Despesa{
             
             if(valor.trim()){
                 valoresArray.push(valor)
-                atributos[i].value = ''
+                
+                //Categorizar modal
+                const modal = new Modal('text-success', 'btn-success', 'Despesa registrada!', 'Despesa registrada com sucesso!');
+
+                console.log(modal)
+
+                modal.categorizarModal(modal)
+                
+                $('#modalRegistro').modal('show');
+
+                // atributos[i].value = '';
             } else{
-                alert('parô, preenche tudo')
-                return;// Mesma coisa que break;
+                $('#modalRegistro').modal('show');
+
+                const modal = new Modal();
+
+                return; // Mesma coisa que break
             }
         }
+
+        console.log(...valoresArray)
 
         const despesas = new Despesa(...valoresArray)
 
         registrarDespesa(despesas, variavelNobj)
 
-        variavelNobj++;
+        variavelNobj++
         localStorage.setItem('variavelNobj', variavelNobj.toString())
         console.log(variavelNobj);
+    }
+}
+
+class Modal {
+    constructor (cT, cB, titulo, descricao){
+        this.classeTexto = cT,
+        this.classeBotao = cB,
+        this.titulo = titulo,
+        this.descricao = descricao
+    }
+
+    categorizarModal(modal){
+        const header = document.getElementById('modal-header');
+        const botao = document.getElementById('btn-modal');
+        const divTitulo = document.getElementById('modalH');
+        const divDescricao = document.getElementById('modal-body');
+
+        header.classList.add(modal.classeTexto)
+        botao.classList.add(modal.classeBotao)
+        divTitulo.textContent = modal.titulo;
+        divDescricao.textContent = modal.descricao;
     }
 }
 
 const registrarDespesa = (d, nD) => {
     console.log(nD)
     localStorage.setItem(`Despesa${nD}`, JSON.stringify(d));
-    console.log(localStorage.getItem(`Despesa ${nD}`))
+    console.log(localStorage.getItem(`Despesa${nD}`))
 }
